ce30b96ff6e2e7b0d41258ea8fefec52c9568f02
Zweisamkeitt <annamarianormann@yandex.ru>
2022-11-07 22:52:41 +0300

    сделала первую дз

---
+++ dz1/script.js
@@ -0,0 +1,9 @@
+var celsius = +prompt("Введите температуру по цельсию:");
+// Tf = (9 / 5) * Tc + 32
+fahrenheit = (9/5) * celsius + 32;
+alert("Температура по Фаренгейту " + fahrenheit);
+
+
+var name, admin;
+name = "Василий";
+alert(name);
